4313c7b7c9fdc719681f31ed9c544f3906f09bfa
Cheonseong Kang <kcs93023@gmail.com>
2018-11-04 01:06:54 +0900

    Add Relu6 Activation function (#3)
    * Add Relu 6 Activate function
    
    * Update Activation.java

--- src/javamachinelearning/utils/Activation.java
+++ src/javamachinelearning/utils/Activation.java
@@ -6,12 +6,12 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t;
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return new Tensor(t.shape(), 1.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Linear";
@@ -23,12 +23,12 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t.map(x -> 1.0 / (1.0 + Math.exp(-x)));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x * (1.0 - x));
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Sigmoid";
@@ -41,12 +41,12 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t.map(x -> Math.min(Math.max(x * 0.2 + 0.5, 0.0), 1.0));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x > 0.0 && x < 1.0 ? 0.2 : 0.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Hard Sigmoid";
@@ -58,12 +58,12 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t.map(x -> 2.0 / (1.0 + Math.exp(-2.0 * x)) - 1.0);
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> 1.0 - x * x);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Hyperbolic Tangent";
@@ -75,12 +75,12 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t.map(x -> Math.max(0.0, x));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x > 0.0 ? 1.0 : 0.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Rectified Linear Unit";
@@ -105,26 +105,43 @@ public interface Activation{
 		}
 	};
 	
+	public static final Activation relu6 = new Activation(){
+		@Override
+		public Tensor activate(Tensor t){
+			return t.map(x -> Math.min(Math.max(0.0, x), 6.0));
+		}
+
+		@Override
+		public Tensor derivative(Tensor t){
+			return t.map(x -> (x > 0.0) && (x < 6.0) ? 1.0 : 0.0);
+		}
+
+		@Override
+		public String toString(){
+			return "Rectified Linear Unit 6";
+		}
+	};
+	
 	public static final Activation softmax = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			Tensor max = t.reduceLast(Double.MIN_VALUE, (a, b) -> Math.max(a, b));
 			max = max.dupLast(t.shape()[t.shape().length - 1]);
-			
+
 			Tensor exp = t.sub(max).map(x -> Math.exp(x));
-			
+
 			Tensor sum = exp.reduceLast(0, (a, b) -> a + b);
 			sum = sum.dupLast(t.shape()[t.shape().length - 1]);
-			
+
 			return exp.div(sum);
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			// because the loss function should be cross entropy
 			return new Tensor(t.shape(), 1.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Softmax";
