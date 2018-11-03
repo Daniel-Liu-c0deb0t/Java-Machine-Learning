42a333b51025c087f02c14a2e5578b5549ab09e4
kcs93023 <kcs93023@gmail.com>
2018-11-03 15:51:12 +0900

    Add Relu 6 Activate function

--- src/javamachinelearning/utils/Activation.java
+++ src/javamachinelearning/utils/Activation.java
@@ -6,87 +6,87 @@ public interface Activation{
 		public Tensor activate(Tensor t){
 			return t;
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return new Tensor(t.shape(), 1.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Linear";
 		}
 	};
-	
+
 	public static final Activation sigmoid = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			return t.map(x -> 1.0 / (1.0 + Math.exp(-x)));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x * (1.0 - x));
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Sigmoid";
 		}
 	};
-	
+
 	// linear approximation of sigmoid
 	public static final Activation hardSigmoid = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			return t.map(x -> Math.min(Math.max(x * 0.2 + 0.5, 0.0), 1.0));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x > 0.0 && x < 1.0 ? 0.2 : 0.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Hard Sigmoid";
 		}
 	};
-	
+
 	public static final Activation tanh = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			return t.map(x -> 2.0 / (1.0 + Math.exp(-2.0 * x)) - 1.0);
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> 1.0 - x * x);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Hyperbolic Tangent";
 		}
 	};
-	
+
 	public static final Activation relu = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			return t.map(x -> Math.max(0.0, x));
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			return t.map(x -> x > 0.0 ? 1.0 : 0.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Rectified Linear Unit";
 		}
 	};
-	
+
 	public static final Activation leakyRelu = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
@@ -104,33 +104,50 @@ public interface Activation{
 			return "Leaky Rectified Linear Unit";
 		}
 	};
-	
+
+	public static final Activation relu6 = new Activation(){
+		@Override
+		public Tensor activate(Tensor t){
+			return t.map(x -> Math.min(Math.max(0.0, x), 6));
+		}
+
+		@Override
+		public Tensor derivative(Tensor t){
+			return t.map(x -> (x > 0.0) && (x < 6.0) ? 1.0 : 0.0);
+		}
+
+		@Override
+		public String toString(){
+			return "Rectified Linear Unit 6";
+		}
+	};
+
 	public static final Activation softmax = new Activation(){
 		@Override
 		public Tensor activate(Tensor t){
 			Tensor max = t.reduceLast(Double.MIN_VALUE, (a, b) -> Math.max(a, b));
 			max = max.dupLast(t.shape()[t.shape().length - 1]);
-			
+
 			Tensor exp = t.sub(max).map(x -> Math.exp(x));
-			
+
 			Tensor sum = exp.reduceLast(0, (a, b) -> a + b);
 			sum = sum.dupLast(t.shape()[t.shape().length - 1]);
-			
+
 			return exp.div(sum);
 		}
-		
+
 		@Override
 		public Tensor derivative(Tensor t){
 			// because the loss function should be cross entropy
 			return new Tensor(t.shape(), 1.0);
 		}
-		
+
 		@Override
 		public String toString(){
 			return "Softmax";
 		}
 	};
-	
+
 	public Tensor activate(Tensor t);
 	// derivatives are calculated in terms of the activated output
 	public Tensor derivative(Tensor t);
